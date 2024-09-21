e1fb4ca5164874b982f22019825896711b0cd02a
XiaoQ <puppy@XiaoQ.local>
2024-09-21 15:58:48 +0800

    simple

---
+++ English/README.md
@@ -0,0 +1,12 @@
+
+#### 2024W38
+- [09-21](../2024W38/09-21/English/README.md)
+- [09-22](../2024W38/09-22/English/README.md)
+
+#### 2024W39
+- [09-23](../2024W39/09-23/English/README.md)
+- [09-24](../2024W39/09-24/English/README.md)
+- [09-25](../2024W39/09-25/English/README.md)
+- [09-26](../2024W39/09-26/English/README.md)
+- [09-27](../2024W39/09-27/English/README.md)
+- [09-28](../2024W39/09-28/English/README.md)
---
+++ today.sh
@@ -0,0 +1,38 @@
+#!/usr/bin/env bash
+off=0
+if [[ $# -gt 0 ]]; then
+    off=$1
+fi
+today="$(date -r $(($(date +%s) + $off * 86400)) +%Y'W'%W'/'%m'-'%d)"
+mkdir -p "${today}"
+pushd "${today}"
+x="README.md"
+if [[ ! -f $x ]]; then
+    install ../../example/TODAY_README.md $x
+fi;
+for x in Lang Math English; do
+    mkdir -p "${x}"
+    pushd "${x}"
+    touch README.md
+    popd
+done
+popd
+wk=${today%%/*}
+touch "${wk}/README.md"
+md_wk="#### ${wk}"
+mmdd=${today##*/}
+for x in Lang Math English; do
+    mkdir -p "${x}"
+    readme="${x}/README.md"
+    touch "${readme}"
+    grep -Fxq "${md_wk}" "${readme}"
+    if [[ 1 -eq $(echo $?) ]]; then
+        echo >>"${readme}"
+        echo "${md_wk}" >>"${readme}"
+    fi
+    md_today='- ['"${mmdd}"'](../'"${today}"'/'"${x}"'/README.md)'
+    grep -Fxq -- "${md_today}" "${readme}"
+    if [[ 1 -eq $(echo $?) ]]; then
+        echo "${md_today}" >>"${readme}"
+    fi
+done
